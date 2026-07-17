179e1802a2812a1862234240cb8f6b7855fb3539
Senapathi Venkata Sri Vinitha <vinithasenapathi075@gmail.com>
2026-07-17 20:49:45 +0530

    Add files via upload

---
+++ week3/spring_REST_using_springboot3/load_country_from_spring_configuration_xml/spring-learn/src/main/java/com/cognizant/springlearn/Country.java
@@ -0,0 +1,49 @@
+package com.cognizant.springlearn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Country {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(Country.class);
+
+    private String code;
+    private String name;
+
+
+    // Empty constructor
+    public Country() {
+        LOGGER.debug("Inside Country Constructor.");
+    }
+
+
+    public String getCode() {
+        LOGGER.debug("Inside getCode method.");
+        return code;
+    }
+
+
+    public void setCode(String code) {
+        LOGGER.debug("Inside setCode method.");
+        this.code = code;
+    }
+
+
+    public String getName() {
+        LOGGER.debug("Inside getName method.");
+        return name;
+    }
+
+
+    public void setName(String name) {
+        LOGGER.debug("Inside setName method.");
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Country [code=" + code + ", name=" + name + "]";
+    }
+}
---
+++ week3/spring_REST_using_springboot3/load_country_from_spring_configuration_xml/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,37 @@
+package com.cognizant.springlearn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringLearnApplication {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringLearnApplication.class);
+
+
+    public static void main(String[] args) {
+
+        displayCountry();
+
+    }
+
+
+    public static void displayCountry() {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+
+        Country country =
+                context.getBean("country", Country.class);
+
+
+        System.out.println(country.toString());
+
+        LOGGER.debug("Country : {}", country.toString());
+
+    }
+
+}
